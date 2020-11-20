94be03352e4bfbde02f7c9c20795b99c727448dc
Mai Ristioja <mai@ristioja.ee>
2020-11-20 17:46:53 +0200

    Implemented header, changed logo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
-  name: 'App',
+
+  name: 'app',
+  methods: {
+
+  },
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: 'Roboto Slab', serif;
+    outline: none;
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    background-color: #0277bd;
+    color: #263238;
+  }
+
+  a {
+    color: #40c4ff;
+  }
+
+  button {
+    padding: 8px 16px;
+    margin: 4px 0;
+    color: #ffffff;
+    background-color: #01579b;
+    border: none;
+    border-radius: 4px;
+  }
+
+  button:hover {
+    box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
+    cursor: pointer;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,129 @@
+<template>
+  <header>
+    <nav>
+      <div class="logo-container">
+        <img src="@/assets/logo.png" alt="postIt">
+      </div>
+      <div class="search-container">
+        <input type="text" name="search"><button type="button">Search</button>
+      </div>
+      <div class="avatar-container">
+        <img class="avatar" @click="toggleProfileMenu" :src="userInfo.avatar">
+        <div class="drop-down-container" v-show="profileMenuShown">
+          <span id="user-name">{{ userInfo.firstname + " " + userInfo.lastname }}</span>
+          <span id="user-email">{{ userInfo.email }}</span>
+          <span class="separator"></span>
+          <span>
+              <a href="browse.html">Browse</a>
+            </span>
+          <span class="separator"></span>
+          <span>
+              <a href="login.html">Log Out</a>
+            </span>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+  name: 'app',
+  data: function() {
+    return {
+      profileMenuShown: false,
+      userInfo: {}
+    }
+  },
+  methods: {
+    toggleProfileMenu: function() {
+      this.profileMenuShown = !this.profileMenuShown
+    },
+    initProfileMenu: function() {
+      axios.get('https://private-517bb-wad20postit.apiary-mock.com/users/1')
+        .then(json => (this.userInfo = json.data))
+    }
+  },
+  beforeMount() {
+    this.initProfileMenu()
+  }
+}
+</script>
+
+<style>
+
+header {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 1;
+}
+
+header:hover {
+  box-shadow: 0 -20px 30px #4d4d4d;
+}
+
+nav {
+  display: flex;
+  background-color: #ffffff;
+  align-items: center;
+}
+
+nav div {
+  height: 30px;
+  flex-grow: 4;
+  padding: 10px;
+}
+
+nav div img {
+  height: 100%;
+  width: 30px;
+  margin-left: 15px;
+  border-radius: 100%;
+  object-fit: cover;
+  object-position: top center;
+}
+
+nav div.search-container > input {
+  box-sizing: border-box;
+  height: 30px;
+  width: 80%;
+  margin: 0;
+  padding: 5px;
+  border: 1px solid #e0e0e0;
+}
+
+nav div.search-container > button {
+  height: 30px;
+  width: 20%;
+  margin: 0;
+  padding: 5px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+nav div.avatar-container {
+  margin-right: 15px;
+  text-align: right;
+}
+.drop-down-container {
+  position: absolute;
+  min-width: 150px;
+  height: auto;
+  background-color: #ffffff;
+  padding: 10px;
+  right: 0;
+  top: 50px;
+  text-align: left;
+}
+.drop-down-container span{
+  display: block;
+}
+.drop-down-container span.separator{
+  border-bottom: 1px solid #d7d7d7;
+  margin: 10px -10px;
+}
+</style>
